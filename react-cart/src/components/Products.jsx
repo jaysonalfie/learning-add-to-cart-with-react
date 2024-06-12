827c7167224a52f5f023f452bbd45ca30ab01887
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-06-12 19:30:42 +0300

    added cart context to allow sharing of data

--- react-cart/src/components/Products.jsx
+++ react-cart/src/components/Products.jsx
@@ -21,6 +21,7 @@ export default function Products() {
           <h1 className="text-2xl uppercase font-bold mt-10 text-center mb-10">
             Shop
           </h1>
+          <h1 className='text-2xl uppercase font-bold mt-10 text-center mb-10'>Cart</h1>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-10">
           {products.map((product) => (
---
+++ react-cart/src/context/cart.jsx
@@ -0,0 +1,99 @@
+import { createContext, useState, useEffect } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+   
+    //initial state to be items we get from the browser
+    const [cartItems, setCartItems] = useState(localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [])
+
+  //adding items to cart
+  //find method to  cehck for items
+  //map function is to increase quantity of the item in cart
+  const addToCart = (item) => {
+    const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id); // check if the item is already in the cart
+
+    if (isItemInCart) {
+      setCartItems(
+        cartItems.map(
+          (
+            cartItem // if the item is already in the cart, increase the quantity of the item
+          ) =>
+            cartItem.id === item.id
+              ? { ...cartItem, quantity: cartItem.quantity + 1 }
+              : cartItem // otherwise, return the cart item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...item, quantity: 1 }]); // if the item is not in the cart, add the item to the cart
+    }
+  };
+
+  //function to remove items from cart
+  //find method to check if item is in cart
+  //filter to remove item from cart
+  //map method to decrease quantity of the item
+  const removeFromCart = (item) => {
+    const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
+
+    if (isItemInCart.quantity === 1) {
+      setCartItems(cartItems.filter((cartItem) => cartItem.id !== item.id)); // if the quantity of the item is 1, remove the item from the cart
+    } else {
+      setCartItems(
+        cartItems.map((cartItem) =>
+          cartItem.id === item.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1 } // if the quantity of the item is greater than 1, decrease the quantity of the item
+            : cartItem
+        )
+      );
+    }
+  };
+
+  //function to clear the cart
+  //sets cart items to empty array
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  //function to get cart total
+  //reduce method  executes reducer function resulting in a single output
+  const getCartTotal = () => {
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    ); // calculate the total price of the items in the cart
+  };
+   
+  //getting cart items from browser
+  //JSON.parse converts items to an object
+  useEffect(() => {
+    const cartItems = localStorage.getItem("cartItems");
+    if (cartItems) {
+      setCartItems(JSON.parse(cartItems));
+    }
+  }, []);
+
+  //persisting cart state in browser
+  //setItem method sets value of specified localstorage item
+  //JSON.stringify converts object into a string
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addToCart,
+        removeFromCart,
+        clearCart,
+        getCartTotal,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+
+  
+
+};
