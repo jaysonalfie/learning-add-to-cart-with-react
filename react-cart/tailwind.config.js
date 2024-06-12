d9141a2fc6053084ec2533f96a629831653bd417
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-06-12 20:18:47 +0300

    tailwind added

--- react-cart/tailwind.config.js
+++ react-cart/tailwind.config.js
@@ -1,14 +1,11 @@
-//tailwind.config.js
-
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./src/**/*.{js,jsx,ts,tsx}",
-];
-export const theme = {
-  extend: {
-    fontFamily: {
-      poppins: ['Poppins', 'sans-serif'],
-    },
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
-};
-export const plugins = [];
+  plugins: [],
+}
